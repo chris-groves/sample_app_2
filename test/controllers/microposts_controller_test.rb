6453cbbac3afcbdfdd852e7197f1f5cb6bc40c13
Chris Groves <chris_groves87@hotmail.co.uk>
2021-03-01 17:30:21 +0000

    destroy microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MicropostsControllerTest < ActionController::TestCase
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @micropost = microposts(:orange)
@@ -18,5 +18,14 @@ end
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
